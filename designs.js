dfd36fa644584660d1a2b1790272899d3091a701
JossFactor <99394940+JossFactor@users.noreply.github.com>
2022-04-25 08:00:21 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -7,9 +7,9 @@ let width = document.getElementById('inputWidth');
 function makeGrid(height, width) {
     table.innerHTML = '';
     // nested loop to create row and cell
-    for (var x = 1; x <= height.value; x++) {
+    for (var x = 0; x < height.value; x++) {
         let row = table.insertRow(x);
-        for (var y = 1; y <= width.value; y++) {
+        for (var y = 0; y < width.value; y++) {
             let cell = row.insertCell(y);
             cell.addEventListener('click', function (event) {
                 event.target.style.backgroundColor = document.getElementById("colorPicker").value;
@@ -19,7 +19,7 @@ function makeGrid(height, width) {
 }
 
 // create the table when the submit button has been clicked
-let sizePicker = document.getElementById('sizePicker');
+const sizePicker = document.getElementById('sizePicker');
 sizePicker.addEventListener('submit', function (event) {
     event.preventDefault();
     makeGrid(height, width);
